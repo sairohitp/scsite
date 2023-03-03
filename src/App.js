6042446fa490e749b08a93f435570b2dc4c36340
Sai Rohit <sairohitp007@gmail.com>
2023-03-04 00:33:16 +0530

    gh corrections v0.03

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 import Home from './pages/home/Home'
 
 const App = () => {
   return (
     <div>
-
-      <BrowserRouter basename = "/scsite">
-        <Routes>
-          <Route exact path = "/scsite" element = {<Home />} />
-        </Routes>
-      </BrowserRouter>
-
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+      </Routes>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 
 import "./index.css"
 
@@ -9,8 +9,8 @@ import App from "./App"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
